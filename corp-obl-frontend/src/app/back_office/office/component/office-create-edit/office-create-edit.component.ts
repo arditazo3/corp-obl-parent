0dcddc1f7c6e31519d3ad48ac471c2107898b101
Ardit Azo <arditazo3@gmail.com>
2018-09-04 16:50:15 +0200

    Back office - Office component

---
+++ corp-obl-frontend/src/app/back_office/office/component/office-create-edit/office-create-edit.component.ts
@@ -0,0 +1,116 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ApiErrorDetails} from '../../../../shared/common/api/model/api-error-details';
+import {Office} from '../../model/office';
+import {SwalComponent} from '@toverux/ngx-sweetalert2';
+import {Router} from '@angular/router';
+import {TransferDataService} from '../../../../shared/common/service/transfer-data.service';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {OfficeService} from '../../service/office.service';
+import {Observable} from '../../../../../../node_modules/rxjs/Rx';
+import {Company} from '../../../company/model/company';
+import {CompanyService} from '../../../company/service/company.service';
+
+@Component({
+  selector: 'app-office-create-edit',
+  templateUrl: './office-create-edit.component.html',
+  styleUrls: ['./office-create-edit.component.css']
+})
+export class OfficeCreateEditComponent implements OnInit {
+
+    isNewForm;
+    office: Office = new Office();
+    submitted = false;
+    errorDetails: ApiErrorDetails;
+
+    companiesObservable: Observable<any[]>;
+    selectedCompany: Company;
+
+    @ViewChild('cancelBtn') cancelBtn;
+    @ViewChild('submitBtn') submitBtn;
+    @ViewChild('errorDescriptionSwal') private errorDescriptionSwal: SwalComponent;
+    @ViewChild('confirmationOfficeSwal') private confirmationOfficeSwal: SwalComponent;
+    createEditOffice: FormGroup;
+
+  constructor(
+      private router: Router,
+      private transferService: TransferDataService,
+      private formBuilder: FormBuilder,
+      private officeService: OfficeService,
+      private companyService: CompanyService
+  ) { }
+
+  ngOnInit() {
+      console.log('OfficeCreateEditComponent - ngOnInit');
+
+      this.office = this.transferService.objectParam;
+
+      if (this.office === undefined) {
+          this.isNewForm = true;
+          this.office = new Office();
+      } else {
+          this.submitBtn.nativeElement.innerText = 'Update office';
+      }
+
+      this.createEditOffice = this.formBuilder.group({
+          description: new FormControl({value: this.office.description, disabled: false}, Validators.required),
+          createdBy: new FormControl({value: this.office.createdBy, disabled: true}),
+          modifiedBy: new FormControl({value: this.office.modifiedBy, disabled: true}),
+          selectedCompanyForm: new FormControl({value: this.selectedCompany, disabled: false}, Validators.required)
+      });
+
+      this.getCompanies();
+  }
+
+    getCompanies() {
+        console.log('OfficeCreateEditComponent - getCompanies');
+
+        const me = this;
+        me.companiesObservable = me.companyService.getCompanies();
+    }
+
+    // convenience getter for easy access to form fields
+    get f() {
+        return this.createEditOffice.controls;
+    }
+
+    createEditOfficeSubmit() {
+        console.log('OfficeCreateEditComponent - createEditOfficeSubmit');
+
+        let me = this;
+        this.submitted = true;
+
+        if (this.createEditOffice.invalid) {
+            return;
+        }
+
+        this.office.description = this.createEditOffice.get('description').value;
+
+        this.confirmationOfficeSwal.title = 'Do you want to save: ' + this.office.description + '?';
+        this.confirmationOfficeSwal.show()
+            .then(function (result) {
+                // handle confirm, result is needed for modals with input
+                me.officeService.saveUpdateOffice(me.office).subscribe(
+                    (data) => {
+                        me.errorDetails = undefined;
+                        console.log('OfficeCreateEditComponent - createEditOfficeSubmit - next');
+                        me.router.navigate(['/back-office/office']);
+                    }, error => {
+                        me.errorDetails = error.error;
+                        me.showErrorDescriptionSwal();
+                        console.log('OfficeCreateEditComponent - createEditOfficeSubmit - error');
+                    }
+                );
+            }, function (dismiss) {
+                // dismiss can be "cancel" | "close" | "outside"
+            });
+    }
+
+    showErrorDescriptionSwal() {
+        console.log('OfficeCreateEditComponent - showErrorDescriptionSwal');
+
+        if (this.errorDetails !== undefined) {
+            this.errorDescriptionSwal.title = 'Seems that ' + this.office.description + ' already exist!';
+            this.errorDescriptionSwal.show();
+        }
+    }
+}
---
+++ corp-obl-frontend/src/app/back_office/office/component/office-table/office-table.component.ts
@@ -0,0 +1,125 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {SwalComponent} from '@toverux/ngx-sweetalert2';
+import {ApiErrorDetails} from '../../../../shared/common/api/model/api-error-details';
+import {Router} from '@angular/router';
+import {TransferDataService} from '../../../../shared/common/service/transfer-data.service';
+import {OfficeService} from '../../service/office.service';
+import {Office} from '../../model/office';
+
+@Component({
+    selector: 'app-office-table',
+    templateUrl: './office-table.component.html',
+    styleUrls: ['./office-table.component.css']
+})
+export class OfficeTableComponent implements OnInit {
+
+    @ViewChild('myTable') table: any;
+    @ViewChild('deleteOfficeSwal') private deleteOfficeSwal: SwalComponent;
+
+    columns: any[];
+    rows: Office[];
+    data: any;
+    temp = [];
+    rowSelected: Office;
+    errorDetails: ApiErrorDetails;
+
+    constructor(
+        private router: Router,
+        private officeService: OfficeService,
+        private transferService: TransferDataService
+    ) {
+    }
+
+    async ngOnInit() {
+        console.log('OfficeTableComponent - ngOnInit');
+
+        const me = this;
+        me.getOffices();
+
+        me.columns = [
+            {prop: 'description', name: 'Description'},
+            {prop: 'createdBy', name: 'Created By'},
+            {prop: 'modifiedBy', name: 'Modified By'}
+        ];
+    }
+
+    getOffices() {
+        console.log('OfficeTableComponent - getOffices');
+
+        const me = this;
+        this.officeService.getOffices().subscribe(
+            (data) => {
+                me.rows = data;
+                me.temp = [...data];
+            }
+        );
+    }
+
+    updateFilter(event) {
+        console.log('OfficeTableComponent - updateFilter');
+
+        const val = event.target.value.toLowerCase();
+
+        // filter our data
+        const temp = this.temp.filter(function (d) {
+            return d.description.toLowerCase().indexOf(val) !== -1 ||
+                d.createdBy.toLowerCase().indexOf(val) !== -1 ||
+                d.modifiedBy.toLowerCase().indexOf(val) !== -1 ||
+                !val;
+        });
+
+        // update the rows
+        this.rows = temp;
+        // Whenever the filter changes, always go back to the first page
+        this.table = this.data;
+    }
+
+    toggleExpandRow(row) {
+        console.log('OfficeTableComponent - Toggled Expand Row!', row);
+        this.table.rowDetail.toggleExpandRow(row);
+    }
+
+    onDetailToggle(event) {
+    }
+
+    createNewOffice() {
+        console.log('OfficeTableComponent - createNewOffice');
+
+        this.router.navigate(['/back-office/office/create']);
+    }
+
+    editOffice(office: Office) {
+        console.log('OfficeTableComponent - editOffice');
+
+        this.transferService.objectParam = office;
+
+        this.router.navigate(['/back-office/office/edit']);
+    }
+
+    deleteOfficeAlert(row) {
+        console.log('OfficeTableComponent - deleteOfficeAlert');
+
+        this.rowSelected = row;
+
+        this.deleteOfficeSwal.title = 'Delete: ' + row.description + '?';
+
+        this.deleteOfficeSwal.show();
+    }
+
+    deleteOfficeCofirm() {
+        console.log('OfficeTableComponent - deleteOfficeCofirm' + this.rowSelected.idOffice);
+
+        const officeSelected = this.rowSelected;
+
+        this.officeService.deleteOffice(officeSelected).subscribe(
+            (data) => {
+                this.errorDetails = undefined;
+                this.getOffices();
+                console.log('OfficeTableComponent - deleteOfficeCofirm - next');
+            }, error => {
+                this.errorDetails = error.error;
+                console.log('OfficeTableComponent - deleteOfficeCofirm - error');
+            }
+        );
+    }
+}
