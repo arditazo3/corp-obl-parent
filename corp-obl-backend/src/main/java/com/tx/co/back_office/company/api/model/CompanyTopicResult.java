8b2351d1ed82b2258294eec3629b8cdebcf193a4
trewpog <arditazo3@gmail.com>
2018-09-07 22:28:33 +0200

    Topic module

---
+++ corp-obl-backend/src/main/java/com/tx/co/back_office/company/api/model/CompanyTopicResult.java
@@ -0,0 +1,40 @@
+package com.tx.co.back_office.company.api.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.tx.co.back_office.company.domain.Company;
+import com.tx.co.back_office.topic.api.model.TopicResult;
+import com.tx.co.back_office.topic.domain.Topic;
+
+/**
+* API model for returning company topic details.
+*
+* @author Ardit Azo
+*/
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CompanyTopicResult {
+
+	private Long idCompanyTopic;
+	private CompanyResult company;
+	private TopicResult topic;
+	
+	public Long getIdCompanyTopic() {
+		return idCompanyTopic;
+	}
+	public void setIdCompanyTopic(Long idCompanyTopic) {
+		this.idCompanyTopic = idCompanyTopic;
+	}
+	public CompanyResult getCompany() {
+		return company;
+	}
+	public void setCompany(CompanyResult company) {
+		this.company = company;
+	}
+	public TopicResult getTopic() {
+		return topic;
+	}
+	public void setTopic(TopicResult topic) {
+		this.topic = topic;
+	}
+	
+	
+}
