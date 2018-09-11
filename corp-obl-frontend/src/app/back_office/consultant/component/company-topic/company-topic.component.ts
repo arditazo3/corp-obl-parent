da4041718d2b8ee2ed49a0938d7a49473452c586
Ardit Azo <arditazo3@gmail.com>
2018-09-12 01:14:51 +0200

    Back office - Consultant module

--- corp-obl-frontend/src/app/back_office/consultant/component/company-topic/company-topic.component.ts
+++ corp-obl-frontend/src/app/back_office/consultant/component/company-topic/company-topic.component.ts
@@ -1,31 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ConsultantService} from '../../service/consultant.service';
 import {TransferDataService} from '../../../../shared/common/service/transfer-data.service';
 import {Router} from '@angular/router';
 import {Observable} from '../../../../../../node_modules/rxjs/Rx';
+import {CompanyTopic} from '../../../company/model/company_topic';
+import {ApiErrorDetails} from '../../../../shared/common/api/model/api-error-details';
 
 @Component({
-  selector: 'app-company-topic',
-  templateUrl: './company-topic.component.html',
-  styleUrls: ['./company-topic.component.css']
+    selector: 'app-company-topic',
+    templateUrl: './company-topic.component.html',
+    styleUrls: ['./company-topic.component.css']
 })
 export class CompanyTopicComponent implements OnInit {
 
-  companyTopicsArray = [];
-  consultantsObservable: Observable<any[]>;
+    companyTopicsArray = [];
+    consultantsObservable: Observable<any[]>;
+    errorDetails: ApiErrorDetails;
 
-  constructor(
-      private router: Router,
-      private companyConsultantService: ConsultantService,
-      private transferService: TransferDataService
-  ) { }
+    constructor(
+        private router: Router,
+        private companyConsultantService: ConsultantService,
+        private transferService: TransferDataService
+    ) {
+    }
 
-  ngOnInit() {
-      console.log('CompanyTopicComponent - ngOnInit');
+    ngOnInit() {
+        console.log('CompanyTopicComponent - ngOnInit');
 
-      const me = this;
-      me.getCompanyTopic(null);
-  }
+        const me = this;
+        me.getCompanyTopic(null);
+    }
 
     getCompanyTopic(selectedCompany) {
         console.log('ConsultantTableComponent - getConsultants');
@@ -50,10 +54,23 @@ export class CompanyTopicComponent implements OnInit {
 
     onAddTopicConsultant(companyTopic, consultant) {
         console.log('CompanyTopicComponent - onAddTopicConsultant');
+        const me = this;
+        this.companyConsultantService.saveUpdateCompanyTopic(companyTopic)
+            .subscribe(
+                (data) => {
+                    me.errorDetails = undefined;
+                    console.log('CompanyTopicComponent - onAddTopicConsultant - next');
+                }, error => {
+                    me.errorDetails = error.error;
+                    console.log('CompanyTopicComponent - onAddTopicConsultant - error');
+                }
+            );
     }
 
     onRemoveTopicConsultant(companyTopic, consultant) {
         console.log('CompanyTopicComponent - onRemoveTopicConsultant');
+
+        this.companyConsultantService.deleteCompanyTopic(companyTopic);
     }
 
 }
