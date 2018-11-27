0cd0c2cc3930a414383659c0169ef35374a8b1d5
trewpog <arditazo3@gmail.com>
2018-11-27 14:49:46 +0100

    1

---
+++ corp-obl-backend/src/main/resources/db_scripts/012_new_translations_mail.sql
@@ -0,0 +1,11 @@
+insert into co_translations (entity_id, tablename, lang, description) VALUES (1, 'expiration#subject#reject', 'EN', 'Rejected task');
+insert into co_translations (entity_id, tablename, lang, description) VALUES (1, 'expiration#subject#reject', 'IT', 'Attività rifiutata');
+
+insert into co_translations (entity_id, tablename, lang, description) VALUES (1, 'expiration#text#reject', 'EN', 'The task has been rejected');
+insert into co_translations (entity_id, tablename, lang, description) VALUES (1, 'expiration#text#reject', 'IT', 'L''attività è stata respinta');
+
+insert into co_translations (entity_id, tablename, lang, description) VALUES (1, 'expiration#subject#expired', 'EN', 'Expiration task');
+insert into co_translations (entity_id, tablename, lang, description) VALUES (1, 'expiration#subject#expired', 'IT', 'Scadenza task');
+
+insert into co_translations (entity_id, tablename, lang, description) VALUES (1, 'expiration#text#expired', 'EN', 'The task has expired');
+insert into co_translations (entity_id, tablename, lang, description) VALUES (1, 'expiration#text#expired', 'IT', 'E'' scaduto il task');
